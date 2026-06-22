246e4d3969b3d74fd8073289bdaa5c62afc5abe6
NguyenLTLinh <justtaketime18@gmail.com>
2026-06-22 14:31:14 +0700

    Update SceneFactory.h

--- include/SceneFactory.h
+++ include/SceneFactory.h
@@ -1,10 +1,1 @@
-#pragma once
 
-#include "Mesh.h"
-
-#include <glm/glm.hpp>
-
-Mesh createAncientBlock(float width, float height, float depth);
-Mesh createColumn(float radius, float height, int segments);
-Mesh createSeaweedBlade(float height, float width, int joints);
-Mesh createBubbleQuad(float size);
